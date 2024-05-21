32df28a64881953363ec008e4bab9177142566b0
Nikita Smolin <nikitkasmo@gmail.com>
2024-05-21 19:37:48 +0300

    updated pages title

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,11 +1,10 @@
-import { Flex, FlexProps } from '@chakra-ui/react'
+import { Flex, FlexProps } from "@chakra-ui/react"
 
 export function Screen({ children, ...restProps }: FlexProps) {
   return (
     <Flex
       direction="column"
       position="fixed"
-      // justifyContent=""
       height="-webkit-fill-available"
       width="100vw"
       overflow="hidden"
